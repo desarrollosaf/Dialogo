b9f9ca7e0178a376419ec12603d04f5557d3c6aa
Nahum <flashjzd58@gmail.com>
2025-09-18 16:28:19 -0600

    cambio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,25 +20,25 @@ export class HomeComponent implements OnInit{
   articulos: any = {
     2024: {
       4: [
-        { titulo: 'Protege tu patrimonio', img: '/patrimonio/img/billete-1-ok-1.png', ruta: '/patrimonio' },
-        { titulo: 'Paridad y la inclusion', img: '/paridad/img/billete-1-ok-1.png', ruta: '/paridad_inclusion' },
+        { titulo: '¿Sabías que el Congreso mexiquense protege tu patrimonio?', img: '/patrimonio/img/billete-1-ok-1.png', ruta: '/patrimonio' },
+        { titulo: 'LXII Legislatura, Legislatura histórica de la Paridad y la inclusión', img: '/paridad/img/billete-1-ok-1.png', ruta: '/paridad_inclusion' },
         // { titulo: '16/ diciembre /2024 ', img: '/img/imagen-entrada-3.png', ruta: '/' },
       ],
     },
     2025: {
       1: [
-        { titulo: 'Reforma Judicial del Edomex', img: '/reforma/img/billete-1-ok-1.png', ruta: '/reforma_judicial' },
-        { titulo: 'Paquete Fiscal 2025', img: 'https://c.animaapp.com/udhLWezT/img/billete-1-ok-1.png', ruta: '/paquete_fiscal' },
-        { titulo: 'Las mujeres al frente del presupuesto', img: '/mujeres/img/billete-1-ok-1.png', ruta: '/mujeres_presupuesto' }
+        { titulo: 'Claves de la Reforma Judicial de Edomex', img: '/reforma/img/billete-1-ok-1.png', ruta: '/reforma_judicial' },
+        { titulo: 'Lo que debes de saber del Paquete Fiscal 2025', img: 'https://c.animaapp.com/udhLWezT/img/billete-1-ok-1.png', ruta: '/paquete_fiscal' },
+        { titulo: 'Las mujeres al frente del presupuesto 2025 en Estado de México', img: '/mujeres/img/billete-1-ok-1.png', ruta: '/mujeres_presupuesto' }
       ],
       2: [
-        { titulo: 'Tala Clandestina', img: '/tala/img/billete-1-ok-1.png', ruta: '/tala_clandestina' },
-        { titulo: 'Maternidad libre', img: '/maternidad/img/billete-1-ok-1.png', ruta: '/maternidad_libre' },
-        { titulo: 'Derecho al ciudadano digno', img: '/ciudadano/img/billete-1-ok-1.png', ruta: '/derecho_al_ciudadano_digno' },
+        { titulo: 'Combate LXII Legislatura mexiquense tala clandestina', img: '/tala/img/billete-1-ok-1.png', ruta: '/tala_clandestina' },
+        { titulo: 'Protege el Congreso Edomex una Maternidad libre', img: '/maternidad/img/billete-1-ok-1.png', ruta: '/maternidad_libre' },
+        { titulo: 'Impulsa LXII  Legislatura derecho al ciudadano digno', img: '/ciudadano/img/billete-1-ok-1.png', ruta: '/derecho_al_ciudadano_digno' },
       ],
       3: [
-        { titulo: 'Bolsas', img: '/img/imagen-entrada-1.png', ruta: '/plastico_edomex' },
-        { titulo: 'Rock mexiquense', img: 'rock/img/billete-1-ok-1.png', ruta: '/rock_mexiquense' },
+        { titulo: 'Dicen adiós a bolsas de plástico en Edomex', img: '/img/imagen-entrada-1.png', ruta: '/plastico_edomex' },
+        { titulo: 'El rock ya tiene su día en el calendario mexiquense', img: 'rock/img/billete-1-ok-1.png', ruta: '/rock_mexiquense' },
       ],
     },
   };
